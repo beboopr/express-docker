1cd5c2d509e4e15a4a78c3d7595d1a066ff8753d
beboopr <linnets-router.09@icloud.com>
2022-11-15 11:37:20 -0500

    working docker container

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000
 const app = express()
 app.use(cors())
 app.get('/test', (req,res) => {
-    res.send('Docker API is running...')
+    res.send('Docker API is pretty cool...')
 })
 
 app.listen(PORT, () => {
